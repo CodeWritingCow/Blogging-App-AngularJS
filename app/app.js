bd7e1974c99f70ed798b3cbe6dd90ee796250961
Gary Pang <gary.pang@gmail.com>
2015-12-09 00:34:57 -0500

    Create home route
    Create AngularJS route and template page for app home. Delete
    unnecessary boilerplate files from AngularJS seed.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
-'use strict';
+(function () {
+    'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider) {
-  // Routes will be here
-}]);
+    // Declare app level module which depends on views, and components
+    angular.module('myApp', [
+    'ngRoute',
+    'myApp.home'                 // Newly add home module
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+        // Set default view of app to home
+
+        $routeProvider.otherwise({
+            redirectTo: '/home'
+        });
+
+    }]);
+
+})();
