eb11b02b60db93edb6104a3e4ba7a043915a929d
Gary Pang <gary.pang@gmail.com>
2015-12-11 03:08:40 -0500

    Update addPost controller
    Add AddPost( ) to AddPostCtrl. Tutorial is outdated ... $firebase
    service has been removed from AngularFire. Need to fix this.

--- app/addPost/addPost.js
+++ app/addPost/addPost.js
@@ -4,15 +4,33 @@
 
     angular.module('myApp.addPost', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/addPost', {
             templateUrl: 'addPost/addPost.html',
             controller: 'AddPostCtrl'
-            });
-        }])
+        });
+    }])
 
-        .controller('AddPostCtrl', ['$scope', function($scope){
-            
+        .controller('AddPostCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
+
+            $scope.AddPost = function () {
+                
+                var firebaseObj = new Firebase('https://blogging-app.firebaseio.com');
+                var fb = $firebase(firebaseObj);
+
+                var title = $scope.article.title;
+                var post = $scope.article.post;
+
+                fb.$push({
+                    title: title,
+                    post: post
+                }).then(function (ref) {
+                    console.log(ref);
+                }, function (error) {
+                    console.log('Error:', error);
+                });
+
+            }
         }]);
 
 })();
