c0793cec5a72a1ce318127dba84205575ee8d07a
Gary Pang <gary.pang@gmail.com>
2015-12-12 22:31:01 -0500

    Create modal window
    Add modal window where users can edit or delete a post.

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -11,11 +11,21 @@
         });
     } ])
 
-    .controller('WelcomeCtrl', ['$scope', 'CommonProp', '$firebaseArray', function ($scope, CommonProp, $firebaseArray) {
+    .controller('WelcomeCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', function ($scope, CommonProp, $firebaseArray, $firebaseObject) {
         $scope.username = CommonProp.getUser();
 
         var firebaseObj = new Firebase('https://blogging-app.firebaseio.com/Articles');
         $scope.articles = $firebaseArray(firebaseObj);
+
+        $scope.editPost = function (id) {
+            var firebaseObj = new Firebase('https://blogging-app.firebaseio.com/Articles/' + id);
+
+            $scope.postToUpdate = $firebaseObject(firebaseObj); // Tutorial uses outdated code. Hope my new ones work!
+
+            $('#editModal').modal(); // triggers the modal pop up
+
+        }
+
 
     } ]);
 
